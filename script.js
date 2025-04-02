b1be1f98a2bc17a718ab91ced140ddc0077e6130
sangeeta-kushwaha <sangeetakushwaha1205@gmail.com>
2025-04-02 15:31:16 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const button = document.getElementById("btn");
+    const usernameInput =document.getElementById("username");
+    const RememberMeCheckBox = document.getElementById("RememberMe");
+    const passwordInput = document.getElementById("password")
+
+
+    let savedUsername = localStorage.getItem("savedUsername") || sessionStorage.getItem("savedUsername");
+    if (savedUsername) {
+        usernameInput.value = savedUsername;
+    }
+    
+
+    button.addEventListener("click",function(){
+        let userName = usernameInput.value.trim();
+        let password = passwordInput.value.trim()
+        let RememberMe = RememberMeCheckBox.checked;
+
+
+        if(userName === "" || password === ""){
+            alert("Please enter both username and password!");
+                    return;
+        }
+
+           
+        if(RememberMe){
+             localStorage.setItem("savedusername", userName);
+    
+          }else{
+             sessionStorage.setItem("savedusername",userName);
+                }
+          alert("login successfully")
+     });
+
+            document.getElementById("loginForm").reset()
+
+           
+  
+        
+    });
+
+
+
+
+
+
